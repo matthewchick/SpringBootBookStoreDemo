790786dfab5d4cff75835cc45cae82b8c6948fd3
Matthew Chick <chikmatthew@gmail.com>
2017-06-09 19:51:46 +1000

    Add how to deploy Heroku and modify the navigation bar

--- src/main/java/com/matthew/bookstore/BookstoreApplication.java
+++ src/main/java/com/matthew/bookstore/BookstoreApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //http://getbootstrap.com/getting-started/#download - download bootstrap and copy them to /resources/static
+//https://devcenter.heroku.com/articles/deploying-spring-boot-apps-to-heroku
+//https://devcenter.heroku.com/articles/renaming-apps
+//1. heroku login
+//2. heroku create
+//3. git push heroku master
+//4. heroku apps:rename matthewbookstore
+//5. heroku git:remote -a matthewbookstore
+//6. heroku open
+//7. https://matthewbookstore.herokuapp.com/
 
 //@Configuration
 //@EnableAutoConfiguration
